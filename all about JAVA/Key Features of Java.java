e2c218ab90fa79cd2ec85fe246f8a8536870b03a
Yuvi_Developer <81914878+yuvi-coder@users.noreply.github.com>
2025-04-17 21:53:49 +0530

    Create Key Features of Java.java

---
+++ all about JAVA/Key Features of Java.java
@@ -0,0 +1,45 @@
+*"Java"*
+
+
+# Platform Independence :
+
+1. Java follows the Write Once, Run Anywhere (WORA) philosophy.
+2. Java programs are compiled into an intermediate form called *bytecode*.
+3. This bytecode can be executed on any platform that has a Java Virtual Machine (JVM).
+4. The JVM is platform-specific, meaning that it adapts Java programs
+to run on different operating systems like Windows, Linux, and macOS.
+
+
+# Automatic Memory Management (Garbage Collection) :
+
+1. Java handles memory management through automatic garbage collection.
+2. This means the JVM automatically manages memory allocation and deallocation,
+freeing up memory that is no longer in use.
+3. This eliminates the need for manual memory management (such as the use of pointers in C/C++).
+
+
+# Multithreading :   // https://www.geeksforgeeks.org/multithreading-in-java/
+
+1. Java provides built-in support for multithreading, allowing developers to
+write highly responsive, parallel programs.
+2. Threads are lightweight processes that can run concurrently.
+3. Java simplifies multithreading by providing a rich set of APIs for creating and managing threads.
+
+
+# Security :
+
+* Java was designed with security in mind.
+* It runs in a "sandbox" to prevent untrusted code from causing harm. - < Java Sandbox > // know more.
+
+""A sandbox is a testing environment that isolates untested code changes and
+outright experimentation from the production environment or repository
+in the context of software development, including web development,
+automation, revision control, configuration management (see also change management), 
+and patch management.""
+
+Key security features in Java include:
+
+1. Bytecode Verification: Ensuring that the bytecode does not perform unsafe operations.
+2. Security Manager: A component of the JVM that controls
+what resources the program can access (e.g., file system, network).
+3. Class Loaders: Load classes dynamically and can impose security checks.
