d7b99cc057a2ee6206603f7a1409735a1408e6ab
Yuvi_Developer <81914878+yuvi-coder@users.noreply.github.com>
2025-04-17 23:25:36 +0530

    Create Java Libraries and Frameworks.java

---
+++ all about JAVA/Java Libraries and Frameworks.java
@@ -0,0 +1,8 @@
+Common Java Libraries and Frameworks :
+
+Java supports various frameworks and libraries that extend its capabilities:
+
+1. Spring Framework: Used for building enterprise-level applications.
+It provides dependency injection and aspect-oriented programming.
+2. Hibernate: An ORM (Object-Relational Mapping) framework that simplifies database operations.
+3. Apache Maven and Gradle: Build automation tools for managing dependencies and building projects.
