a96792f9db8848e6500c6ccad40390690a8ac21f
Yuvi_Developer <81914878+yuvi-coder@users.noreply.github.com>
2025-04-22 22:40:39 +0530

    Create Class and Object.java

---
+++ all about JAVA/Class and Object.java
@@ -0,0 +1,47 @@
+class and objects :
+
+class : 
+Class is the blueprint of an object. It is used to create objects.
+No memory is allocated when a class is declared.
+A Class in Java can contain:
+                                    1. Data member
+                                    2. Method
+                                    3. Constructor
+                                    4. Nested Class
+                                    5. Interface
+                                    
+Modifiers: A class can be public or has default access (Refer this for details).
+
+Class keyword: Class keyword is used to create a class.
+
+Class name: The name should begin with an initial letter (capitalized by convention).
+
+Superclass (if any): The name of the class’s parent (superclass), if any, preceded
+by the keyword extends. A class can only extend (subclass) one parent.
+Interfaces(if any): A comma-separated list of interfaces implemented by
+the class, if any, preceded by the keyword implements. A class
+can implement more than one interface.
+Body: The class body is surrounded by braces, { }.
+
+Subclass in Java :
+1. A subclass is a class that inherits from another class, known asthe superclass or parent class.
+2. The subclass can access and use the properties
+(fields) and methods of the superclass, but it can also add its own fields and
+methods or override existing ones.
+3. Inheritance allows you to create a new class that reuses the code from an existing class.
+The extends keyword is used to define a subclass in Java.
+
+
+ //https://www.geeksforgeeks.org/classes-objects-java/  -  IMPORTANT TO KNOW
+
+objects : 
+An object is an instance of the class.
+Memory is allocated as soon as an object is created.
+
+Java object consists : 
+                                   1. State: It is represented by attributes of an object. It also reflects 
+                                   the properties of an object.
+                                   2. Behavior: It is represented by the methods of an object. It also
+                                   reflects the response of an object with other objects.
+                                   3. Identity: It gives a unique name to an object and enables one object
+                                   to interact with other objects.
